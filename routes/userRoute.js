c30e15f5c29344e609a36166db83519df114e847
King <tshimologokingmabena@gmail.com>
2022-07-29 13:32:21 +0200

    2nd Update

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,111 @@
 const express = require("express");
 const router = express.Router();
 const con = require("../library/db_connec");
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+
+//Register Route
+//The Route where Encryption starts
+router.post("/register", (req, res) => {
+  try {
+    let sql = "INSERT INTO users SET ?";
+    const {
+      full_name,
+      email,
+      password,
+      user_type,
+      phone,
+      country,
+      billing_address,
+      default_shipping_address,
+    } = req.body;
+
+    //Start of Hashing/Encryption
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    let user = {
+      full_name,
+      email,
+
+      //Sending the has value to be stored within the table
+      password: hash,
+      user_type,
+      phone,
+      country,
+      billing_address,
+      default_shipping_address,
+    };
+
+
+    con.query(sql, user, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      res.send(`User ${(user.full_name, user.email)} created Successfully`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//Login
+router.post("/login", (req, res) => {
+  try{
+    let sql = "SELECT * FROM users WHERE ?";
+    let user = {
+      email: req.body.email,
+    };
+    con.query(sql, user, async (err, result) => {
+      if (err) throw err;
+      if(result.length === 0) {
+        res.send("Email not found please register");
+      }
+      else{
+        //Decryption
+        //Accepts the password stored in the db and the password given by the user(req.body)
+        const isMatch = await bcrypt.compare(
+          req.body.password,
+          result[0].password
+        );
+
+        //If the password does not match
+        if(!isMatch) {
+          res.send("Password is Incorrect");
+        }
+        else{
+          const payload = {
+            user: {
+              user_id: result[0].user_id,
+              full_name: result[0].full_name,
+              email: result[0].email,
+              user_type: result[0].user_type,
+              phone: result[0].phone,
+              country: result[0].country,
+              billing_address: result[0].billing_address,
+              default_shipping_address: result[0].default_shipping_address,
+            },
+          }
+        };
+
+        //Creating a token and setting an expiry date
+        jwt.sign(
+          payload,
+          process.env.jwtSecret,
+          {
+            expiresIn: "365d",
+          },
+          (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+          }
+        );
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 router.get("/", (req, res) => {
   try {
     con.query("SELECT * FROM users", (err, result) => {
