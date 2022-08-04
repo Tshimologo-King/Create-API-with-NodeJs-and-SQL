6b79c5c11b18339df7e2c3002b4b12b1a217b903
King <tshimologokingmabena@gmail.com>
2022-08-04 11:38:27 +0200

    ...

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.static("Public"));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/" + "index.html");
+  res.sendFile(__dirname + "/" + "public/index.html");
 });
 
 //use routes
